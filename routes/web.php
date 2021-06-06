65544f4fc5224a1763b3c4da748caa29c49ac418
rintaro uchida <v795b3eed7qzaE05SE@softbank.ne.jp>
2021-06-07 07:07:02 +0900

    fix:basic-information

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'ArticleController@index');
+Route::get('/', [ArticleController::class, 'index']);
 
 Auth::routes();
 
