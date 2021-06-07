f7cdb7f82569da57103a3c88fea4f19ea025bce3
rintaro uchida <v795b3eed7qzaE05SE@softbank.ne.jp>
2021-06-07 17:33:10 +0900

    feat:password-reset

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,6 +29,9 @@
 
                 {{--ここから--}}
                 <input type="hidden" name="remember" id="remember" value="on">
+                <div class="text-left">
+                    <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                </div>
                 {{--ここまで--}}
 
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
